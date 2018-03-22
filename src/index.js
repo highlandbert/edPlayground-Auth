44f987041d25a21bbf986a9104bdfefc0b326aa1
unknown <roberto.sl.90@gmail.com>
2018-03-23 00:31:23 +0100

    fontawesome with no cdn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import '../node_modules/bulma/css/bulma.css'
+import '../node_modules/@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
+import '../node_modules/@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
+import '../node_modules/@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
 import './index.css'
 
 import React from 'react'
